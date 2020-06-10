3694c36cf9384d0b0c6362ced981808fd8072015
Skyf0l <tom.rorato@epitech.eu>
2020-06-10 20:27:13 +0200

    Add display loop and preview of base64

--- basecracker.py
+++ basecracker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import base64
 import sys
 import re
 
@@ -11,13 +12,13 @@ def base16_encoder(plaintext):
 def base16_decoder(cipher):
     return cipher
 def base64_encoder(plaintext):
-    return plaintext
+    return base64.b64encode(bytearray(plaintext, 'utf8')).decode("utf-8")
 def base64_decoder(cipher):
-    return cipher
+    return base64.b64decode(cipher).decode("utf-8")
 
 # base tab
 all_bases = [
-    ['16', 'hexadecimal', base16_encoder, base16_decoder],
+    ['16', 'base16', base16_encoder, base16_decoder],
     ['64', 'base64', base64_encoder, base64_decoder]
 ]
 ENCODER = 2
@@ -33,35 +34,47 @@ def get_base_data(base_name):
 # main encoder
 def main_encoder(plaintext, bases):
     cipher = plaintext
+    print('Plaintext: ' + plaintext)
+    print()
 
     for base in bases:
         base_data = get_base_data(base)
         if base_data is None:
-            print('unknown base: ' + base + ' (ignored)')
+            print('Unknown base: ' + base + ' (ignored)')
             continue
 
         cipher = base_data[ENCODER](cipher)
         if cipher is None:
-            print('error while encoding in base ' + base_data[0] + ' (' + base_data[1] + ')')
+            print('Error while encoding in ' + base_data[1])
             return None
 
+        print('Apply ' + base_data[1] + ': ' + cipher)
+
+    print()
+    print('Cipher: ' + cipher)
     return cipher
 
 # main decoder
 def main_decoder(cipher, bases):
     plaintext = cipher
+    print('Cipher: ' + cipher)
+    print()
 
     for base in bases:
         base_data = get_base_data(base)
         if base_data is None:
-            print('unknown base: ' + base + ' (ignored)')
+            print('Unknown base: ' + base + ' (ignored)')
             continue
 
         plaintext = base_data[DECODER](plaintext)
         if plaintext is None:
-            print('error while decoding in base ' + base_data[0] + ' (' + base_data[1] + ')')
+            print('Error while decoding in base ' + base_data[0] + ' (' + base_data[1] + ')')
             return None
 
+        print('Apply ' + base_data[1] + ': ' + plaintext)
+
+    print()
+    print('Plaintext: ' + plaintext)
     return plaintext
 
 # main cracker
@@ -101,19 +114,19 @@ def print_help():
     print('Usage:')
     print('    ' + name + ' cipher')
     print('     ->try to crack cipher')
-    print('')
+    print()
     print('    ' + name + ' -e plaintext base_names')
     print('     -> encode cipher in bases')
     print('    ' + name + ' -d plaintext base_names')
     print('     -> decode cipher from bases')
-    print('')
+    print()
     print('    base_names can be stacked and are applied in order (space and coma are delimiters)')
     print('    base_names supported are:')
     for base in all_bases:
         print('        ' + base[0] + '\talias ' + base[1])
-    print('')
+    print()
     print('Exemple:')
-    print('    $ ' + name + ' -e basecracker \'64 hexadecimal\'')
+    print('    $ ' + name + ' -e basecracker \'64 base16\'')
     print('     -> 596d467a5a574e7959574e725a58493d')
     print('    $ ' + name + ' -d 596d467a5a574e7959574e725a58493d \'16,base64\'')
     print('     -> basecracker')
