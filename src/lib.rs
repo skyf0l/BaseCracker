4b1d416cf2837310e31bcc492df3ca7ec5f3b1ad
Skyf0l <tom.rorato@epitech.eu>
2021-12-01 17:56:33 +0100

    Udpate lib and Add hex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-mod basecracker;
 pub mod modules;
 
-pub use basecracker::basecracker;
+pub fn basecracker(cipher: &String) -> String {
+    let mut result = cipher.clone();
+    let bases = modules::get_bases();
+
+    for base in bases {
+        match base.encode(&result) {
+            Ok(plain) => result = plain,
+            Err(e) => println!("{}", e),
+        }
+    }
+    result
+}
