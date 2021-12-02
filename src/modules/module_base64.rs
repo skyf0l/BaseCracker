09bb5c5bb51cf95e7dd44278b9f15ca88ecfdfc1
Skyf0l <tom.rorato@epitech.eu>
2021-12-02 16:10:44 +0100

    Update architecture

--- src/modules/module_base64.rs
+++ src/modules/module_base64.rs
@@ -3,11 +3,17 @@ pub struct Base64;
 use super::Base;
 
 impl Base for Base64 {
-    fn encode(&self, decoded: &String) -> Result<String, Box<dyn std::error::Error>> {
+    fn get_name(&self) -> &'static str {
+        "base64"
+    }
+    fn get_short_name(&self) -> &'static str {
+        "b64"
+    }
+    fn encode(&self, decoded: &str) -> Result<String, Box<dyn std::error::Error>> {
         println!("base64 encode: {}", decoded);
         Ok(decoded.to_string())
     }
-    fn decode(&self, encoded: &String) -> Result<String, Box<dyn std::error::Error>> {
+    fn decode(&self, encoded: &str) -> Result<String, Box<dyn std::error::Error>> {
         println!("base64 decode: {}", encoded);
         Ok(encoded.to_string())
     }
--- src/modules/module_hex.rs
+++ src/modules/module_hex.rs
@@ -3,14 +3,20 @@ pub struct Hex;
 use super::Base;
 
 impl Base for Hex {
-    fn encode(&self, decoded: &String) -> Result<String, Box<dyn std::error::Error>> {
+    fn get_name(&self) -> &'static str {
+        "hex"
+    }
+    fn get_short_name(&self) -> &'static str {
+        "h"
+    }
+    fn encode(&self, decoded: &str) -> Result<String, Box<dyn std::error::Error>> {
         let mut encoded = String::new();
         for c in decoded.chars() {
             encoded.push_str(&format!("{:02x}", c as u8));
         }
         Ok(encoded)
     }
-    fn decode(&self, encoded: &String) -> Result<String, Box<dyn std::error::Error>> {
+    fn decode(&self, encoded: &str) -> Result<String, Box<dyn std::error::Error>> {
         let mut decoded = String::new();
         for i in (0..encoded.len()).step_by(2) {
             let c = u8::from_str_radix(&encoded[i..i + 2], 16)?;
