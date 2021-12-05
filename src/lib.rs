915e47b208cff9eec71f399a96f4351ad1b08e05
Skyf0l <tom.rorato@epitech.eu>
2021-12-05 21:54:07 +0100

    Update basecracker core

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,32 @@
 pub mod modules;
 
+fn get_printable_percentage(s: &str) -> f32 {
+    let mut sum = 0.0;
+    for c in s.chars() {
+        if c.is_alphabetic() || ((c as u32) >= 32 && (c as u32) < 127) {
+            sum += 1.0;
+        }
+    }
+    sum / s.len() as f32
+}
+
 pub fn basecracker(cipher: &String) -> String {
-    let mut result = cipher.clone();
+    let mut result = String::new();
     let bases = modules::get_bases();
 
     for base in bases {
-        match base.encode(&result) {
-            Ok(plain) => result = plain,
-            Err(e) => println!("{}", e),
+        match base.decode(&cipher) {
+            Ok(plain) => {
+                result = plain;
+                println!(
+                    "{}: {:.2}%: {}",
+                    base.get_name(),
+                    get_printable_percentage(result.as_str()) * 100.0,
+                    result
+                );
+            }
+            Err(e) => println!("{}: {}", base.get_name(), e),
         }
     }
-    println!("{}", result);
     result
 }
