1f7ab7902be6f05df215ed5e93027769896a1d99
Skyf0l <tom.rorato@epitech.eu>
2021-12-05 22:17:32 +0100

    Add base58 and base62

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,5 +1,7 @@
 pub mod module_base2;
 pub mod module_base32;
+pub mod module_base58;
+pub mod module_base62;
 pub mod module_base64;
 pub mod module_hex;
 
@@ -20,6 +22,8 @@ pub fn get_bases() -> Vec<Box<dyn Base>> {
         Box::new(module_base2::Base2),
         Box::new(module_hex::Hex),
         Box::new(module_base32::Base32),
+        Box::new(module_base58::Base58),
+        Box::new(module_base62::Base62),
         Box::new(module_base64::Base64),
     ];
     bases
---
+++ src/modules/module_base58.rs
@@ -0,0 +1,83 @@
+pub struct Base58;
+
+use super::*;
+
+impl Base for Base58 {
+    fn get_name(&self) -> &'static str {
+        "base58"
+    }
+    fn get_short_name(&self) -> &'static str {
+        "b58"
+    }
+    fn get_base(&self) -> &'static str {
+        "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    }
+    fn get_padding(&self) -> Option<&'static str> {
+        None
+    }
+    fn encode(&self, decoded: &str) -> Result<String, Box<dyn std::error::Error>> {
+        encode_decimal(decoded, self.get_base(), 1)
+    }
+    fn decode(&self, encoded: &str) -> Result<String, Box<dyn std::error::Error>> {
+        decode_decimal(encoded, self.get_base())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode_1() {
+        let base = Base58;
+        assert_eq!(
+            base.encode(&String::from("Hello World!")).unwrap(),
+            "2NEpo7TZRRrLZSi2U"
+        );
+    }
+
+    #[test]
+    fn test_encode_2() {
+        let base = Base58;
+        assert_eq!(base.encode(&String::from("a")).unwrap(), "2g");
+    }
+
+    #[test]
+    fn test_encode_3() {
+        let base = Base58;
+        assert_eq!(base.encode(&String::from("ab")).unwrap(), "8Qq");
+    }
+
+    #[test]
+    fn test_encode_4() {
+        let base = Base58;
+        assert_eq!(base.encode(&String::from("abc")).unwrap(), "ZiCa");
+    }
+
+    #[test]
+    fn test_decode_1() {
+        let base = Base58;
+        assert_eq!(
+            base.decode(&String::from("2NEpo7TZRRrLZSi2U")).unwrap(),
+            "Hello World!"
+        );
+    }
+
+    #[test]
+    fn test_decode_2() {
+        let base = Base58;
+        assert_eq!(base.decode(&String::from("2g")).unwrap(), "a");
+    }
+
+    #[test]
+    fn test_decode_3() {
+        let base = Base58;
+        assert_eq!(base.decode(&String::from("8Qq")).unwrap(), "ab");
+    }
+
+    #[test]
+    fn test_decode_4() {
+        let base = Base58;
+        assert_eq!(base.decode(&String::from("ZiCa")).unwrap(), "abc");
+    }
+}
