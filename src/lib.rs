803f2eb521e7207fffc11faccaa171503c246084
Skyf0l <tom.rorato@epitech.eu>
2022-01-24 20:16:05 +0100

    Add check if len == 0

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod modules;
 
 fn get_printable_percentage(s: &str) -> f32 {
+    if s.len() == 0 {
+        return 0.0;
+    }
     let mut sum = 0.0;
     for c in s.chars() {
         if c.is_alphabetic() || ((c as u32) >= 32 && (c as u32) < 127) {
