af2af8a7882f274d5bab4824ea4620b12d4cd224
Skyf0l <tom.rorato@epitech.eu>
2022-03-09 12:48:58 +0100

    Add json generation for special display

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,56 @@
 use basecracker;
 
+/// Convert result of basecracker in json format:
+///
+/// ```json
+/// {
+///   "cipher": ...,
+///   "plaintexts": [
+///     {
+///       "plaintext": ...,
+///       "bases": [
+///         ...,
+///         ...
+///       ]
+///     },
+///     ...
+///   ]
+/// }
+/// ```
+fn plaintexts_to_json(cipher: &str, plaintexts: &Vec<(String, Vec<String>)>) -> String {
+    let mut json = String::new();
+    json.push_str("{\n");
+    json.push_str("  \"cipher\": \"");
+    json.push_str(cipher);
+    json.push_str("\",\n");
+    json.push_str("  \"plaintexts\": [\n");
+    for (i, (plaintext, bases)) in plaintexts.iter().enumerate() {
+        json.push_str("    {\n");
+        json.push_str("      \"plaintext\": \"");
+        json.push_str(plaintext);
+        json.push_str("\",\n");
+        json.push_str("      \"bases\": [\n");
+        for (j, base) in bases.iter().enumerate() {
+            json.push_str("        \"");
+            json.push_str(base);
+            json.push_str("\"");
+            if j < bases.len() - 1 {
+                json.push_str(",");
+            }
+            json.push_str("\n");
+        }
+        json.push_str("      ]\n");
+        json.push_str("    }");
+        if i < plaintexts.len() - 1 {
+            json.push_str(",");
+        }
+        json.push_str("\n");
+    }
+    json.push_str("  ]\n");
+    json.push_str("}");
+    json
+}
+
 fn main() {
     // get args
     let args: Vec<String> = std::env::args().collect();
@@ -9,5 +60,10 @@ fn main() {
         println!("Usage: {} <cipher>", args[0]);
         return;
     }
-    basecracker::basecracker(&args[1]);
+    let plaintexts = basecracker::basecracker(&args[1]);
+    println!("{}", plaintexts_to_json(&args[1], &plaintexts));
+    for (plaintext, bases) in plaintexts {
+        println!("Decode order: {}", bases.join(" -> "));
+        println!("Plaintext: {}", plaintext);
+    }
 }
