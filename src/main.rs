f215818e2ef4c171b0dfd7ab6dbbce534ad658a6
Skyf0l <tom.rorato@epitech.eu>
2022-03-09 16:30:14 +0100

    Add argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use basecracker;
+use clap::Parser;
 
 /// Convert result of basecracker in json format:
 ///
@@ -51,19 +52,103 @@ fn plaintexts_to_json(cipher: &str, plaintexts: &Vec<(String, Vec<String>)>) -> 
     json
 }
 
-fn main() {
-    // get args
-    let args: Vec<String> = std::env::args().collect();
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    /// Encode given plaintext using the specified bases
+    #[clap(
+        short,
+        long,
+        value_name = "PLAINTEXT",
+        conflicts_with_all = &["decode", "crack"]
+    )]
+    encode: Option<String>,
 
-    // check args
-    if args.len() != 2 {
-        println!("Usage: {} <cipher>", args[0]);
-        return;
-    }
-    let plaintexts = basecracker::basecracker(&args[1]);
-    println!("{}", plaintexts_to_json(&args[1], &plaintexts));
-    for (plaintext, bases) in plaintexts {
-        println!("Decode order: {}", bases.join(" -> "));
-        println!("Plaintext: {}", plaintext);
+    /// Decode given cipher using the specified bases
+    #[clap(
+        short,
+        long,
+        value_name = "CIPHER",
+        conflicts_with_all = &["encode", "crack"]
+    )]
+    decode: Option<String>,
+
+    /// Try to crack the cipher using the specified bases (default: all)
+    #[clap(
+        short,
+        long,
+        value_name = "CIPHER",
+        conflicts_with_all = &["encode", "decode"]
+    )]
+    crack: Option<String>,
+
+    /// Set base to use for encoding/decoding
+    #[clap(
+        short,
+        long, 
+        min_values = 1,
+        requires_all = &["encode", "decode", "crack"]
+    )]
+    bases: Option<Vec<String>>,
+
+    /// Output cracker results in json format
+    #[clap(
+        short,
+        long,
+        requires = "crack",
+        conflicts_with_all = &["verbose", "quiet"]
+    )]
+    json: bool,
+
+    /// Verbose mode, print encoding/decoding steps
+    #[clap(short, long, conflicts_with_all = &["quiet", "json"])]
+    verbose: bool,
+
+    /// Quiet mode, don't print anything except results
+    #[clap(short, long, conflicts_with_all = &["verbose", "json"])]
+    quiet: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if let Some(_plaintext) = args.encode {
+        let _bases = args.bases;
+        todo!();
+    } else if let Some(_cipher) = args.decode {
+        let _bases = args.bases;
+        todo!()
+    } else if let Some(cipher) = args.crack {
+        let _bases = args.bases;
+        let plaintexts = basecracker::basecracker(&cipher);
+
+        if args.json {
+            println!("{}", plaintexts_to_json(&cipher, &plaintexts));
+            // if no plaintexts were found, exit with error code
+            if plaintexts.len() == 0 {
+                std::process::exit(1);
+            }
+        } else {
+            if plaintexts.len() != 0 {
+                for (plaintext, bases) in plaintexts {
+                    if !args.quiet {
+                        println!("Decode order: {}", bases.join(" -> "));
+                        println!("Plaintext: {}", plaintext);
+                    } else {
+                        println!("{}", plaintext);
+                    }
+                }
+            } else {
+                // if no plaintexts were found, display error and exit with error code
+                if !args.quiet {
+                    eprintln!("No plaintexts found");
+                }
+                std::process::exit(1);
+            }
+        }
+    } else {
+        eprintln!("No action specified");
+        eprintln!("For more information try --help");
+        std::process::exit(1);
     }
 }
