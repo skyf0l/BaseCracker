5cb8e1ab5139a8b2f1011db7f6e85560a2cd24a1
Skyf0l <tom.rorato@epitech.eu>
2022-03-10 16:35:35 +0100

    Fix decode order and add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,25 @@ use modules::Base;
 pub mod utils;
 pub use utils::get_printable_percentage;
 
-pub fn decode_round(
+pub fn encode(cipher: &str, bases: &Vec<Box<dyn Base>>) -> Result<String, String> {
+    let mut result = cipher.to_string();
+
+    for base in bases {
+        result = base.encode(&result)?;
+    }
+    Ok(result)
+}
+
+pub fn decode(cipher: &str, bases: &Vec<Box<dyn Base>>) -> Result<String, String> {
+    let mut result = cipher.to_string();
+
+    for base in bases {
+        result = base.decode(&result)?;
+    }
+    Ok(result)
+}
+
+pub fn crack_round(
     cipher: &str,
     bases: &Vec<Box<dyn Base>>,
     min_printable_percentage: f32,
@@ -25,24 +43,6 @@ pub fn decode_round(
         }
     }
     result
-}
-
-pub fn encode(cipher: &str, bases: &Vec<Box<dyn Base>>) -> Result<String, String> {
-    let mut result = cipher.to_string();
-
-    for base in bases {
-        result = base.encode(&result)?;
-    }
-    Ok(result)
-}
-
-pub fn decode(cipher: &str, bases: &Vec<Box<dyn Base>>) -> Result<String, String> {
-    let mut result = cipher.to_string();
-
-    for base in bases {
-        result = base.decode(&result)?;
-    }
-    Ok(result)
 }
 
 pub fn basecracker(cipher: &str) -> Vec<(String, Vec<String>)> {
@@ -65,7 +65,7 @@ pub fn basecracker_with_bases(
 
     // initialize the results vector
     let mut results = Vec::new();
-    let round_result = decode_round(cipher, &bases, min_printable_percentage);
+    let round_result = crack_round(cipher, &bases, min_printable_percentage);
     for (plain, base) in round_result {
         results.push((plain, vec![base]));
     }
@@ -74,7 +74,7 @@ pub fn basecracker_with_bases(
     while results.len() > 0 {
         let mut next_results = Vec::new();
         for (plain, base_names) in results {
-            let round_result = decode_round(&plain, &bases, min_printable_percentage);
+            let round_result = crack_round(&plain, &bases, min_printable_percentage);
             if round_result.len() == 0 {
                 final_result.push((plain, base_names.clone()));
             } else {
@@ -104,6 +104,28 @@ mod tests {
     }
 
     #[test]
+    fn test_encode_base64() {
+        assert_eq!(
+            encode(
+                "Hello World!",
+                &get_bases_from_names(&vec!["base64".to_string()]).unwrap()
+            ),
+            Ok("SGVsbG8gV29ybGQh".to_string())
+        );
+    }
+
+    #[test]
+    fn test_decode_base64() {
+        assert_eq!(
+            decode(
+                "SGVsbG8gV29ybGQh",
+                &get_bases_from_names(&vec!["base64".to_string()]).unwrap()
+            ),
+            Ok("Hello World!".to_string())
+        );
+    }
+
+    #[test]
     fn test_crack_base64_x2() {
         assert_eq!(
             basecracker("U0dWc2JHOGdWMjl5YkdRaA=="),
@@ -111,6 +133,28 @@ mod tests {
                 "Hello World!".to_string(),
                 vec!["base64".to_string(), "base64".to_string()]
             )]
+        );
+    }
+
+    #[test]
+    fn test_encode_base64_x2() {
+        assert_eq!(
+            encode(
+                "Hello World!",
+                &get_bases_from_names(&vec!["base64".to_string(), "base64".to_string()]).unwrap()
+            ),
+            Ok("U0dWc2JHOGdWMjl5YkdRaA==".to_string())
+        );
+    }
+
+    #[test]
+    fn test_decode_base64_x2() {
+        assert_eq!(
+            decode(
+                "U0dWc2JHOGdWMjl5YkdRaA==",
+                &get_bases_from_names(&vec!["base64".to_string(), "base64".to_string()]).unwrap()
+            ),
+            Ok("Hello World!".to_string())
         );
     }
 
@@ -130,9 +174,41 @@ mod tests {
     }
 
     #[test]
+    fn test_encode_base64_x3() {
+        assert_eq!(
+            encode(
+                "Hello World!",
+                &get_bases_from_names(&vec![
+                    "base64".to_string(),
+                    "base64".to_string(),
+                    "base64".to_string()
+                ])
+                .unwrap()
+            ),
+            Ok("VTBkV2MySkhPR2RXTWpsNVlrZFJhQT09".to_string())
+        );
+    }
+
+    #[test]
+    fn test_decode_base64_x3() {
+        assert_eq!(
+            decode(
+                "VTBkV2MySkhPR2RXTWpsNVlrZFJhQT09",
+                &get_bases_from_names(&vec![
+                    "base64".to_string(),
+                    "base64".to_string(),
+                    "base64".to_string()
+                ])
+                .unwrap()
+            ),
+            Ok("Hello World!".to_string())
+        );
+    }
+
+    #[test]
     fn test_crack_base64_hex_base58() {
         assert_eq!(
-            basecracker("SeM5ddt2Lgxo2LJCZskRpa86BBPR7L13PffVDCP6FaCF73vnp5kCU4euqUfd18GP"),
+            basecracker("4afto9ow5ffzGyMCjboUeq5HbDkPTXpqPX4NMBwUCypB"),
             vec![(
                 "Hello World!".to_string(),
                 vec![
@@ -141,6 +217,38 @@ mod tests {
                     "base64".to_string()
                 ]
             )]
+        );
+    }
+
+    #[test]
+    fn test_encode_base64_hex_base58() {
+        assert_eq!(
+            encode(
+                "Hello World!",
+                &get_bases_from_names(&vec![
+                    "base64".to_string(),
+                    "hex".to_string(),
+                    "base58".to_string()
+                ])
+                .unwrap()
+            ),
+            Ok("4afto9ow5ffzGyMCjboUeq5HbDkPTXpqPX4NMBwUCypB".to_string())
+        );
+    }
+
+    #[test]
+    fn test_decode_base64_hex_base58() {
+        assert_eq!(
+            decode(
+                "4afto9ow5ffzGyMCjboUeq5HbDkPTXpqPX4NMBwUCypB",
+                &get_bases_from_names(&vec![
+                    "base58".to_string(),
+                    "hex".to_string(),
+                    "base64".to_string()
+                ])
+                .unwrap()
+            ),
+            Ok("Hello World!".to_string())
         );
     }
 }
